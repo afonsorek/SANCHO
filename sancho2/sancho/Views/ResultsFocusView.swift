efe290aa1caee9caa029d23b7128192d21bda5b6
Afonso Rekbaim <afonso.rekbaim@pucpr.edu.br>
2023-06-28 17:19:10 -0300

    Simplificação prompt e resposta GPT

---
+++ sancho2/sancho/Views/ResultsFocusView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultsFocusView.swift
+//  sancho
+//
+//  Created by Afonso Rekbaim on 28/06/23.
+//
+
+import SwiftUI
+
+struct ResultsFocusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultsFocusView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultsFocusView()
+    }
+}
