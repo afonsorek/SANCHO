ffd8afb95881a0242e019f05a316cb8f9ffb7f33
Afonso Rekbaim <afonso.rekbaim@pucpr.edu.br>
2023-07-03 14:26:42 -0300

    Mudanças Focus View para confirmação de dados

--- sancho2/sancho/Views/ResultsFocusView.swift
+++ sancho2/sancho/Views/ResultsFocusView.swift
@@ -1,11 +1,3 @@
-
-//
-//  ResultsFocus.swift
-//  sancho
-//
-//  Created by Jady Torralvo on 27/06/23.
-//
-
 import Foundation
 import SwiftUI
 
@@ -13,24 +5,30 @@ struct ResultsFocusView: View {
     var title: String
     var steps: [String]
     var descs: [String]
+    
+    @State private var checkedStates: [Bool]
+    
+    init(title: String, steps: [String], descs: [String]) {
+        self.title = title
+        self.steps = steps
+        self.descs = descs
+        self._checkedStates = State(initialValue: Array(repeating: false, count: steps.count))
+    }
+    
     var body: some View {
-        
-        // ZStack: lvl 1 -> título + navbar + main button // lvl 2 -> HStack cards
-        NavigationView{
-            ZStack{
+        NavigationView {
+            ZStack {
                 VStack {
                     HStack(spacing: 16) {
-                        NavigationLink{
-                            AskInputView()
-                        } label:{
+                        NavigationLink(destination: AskInputView()) {
                             Image(systemName: "character.bubble.fill")
                                 .padding()
                                 .frame(width: 36, height: 36)
                                 .background(Color(uiColor: .systemGray5))
                                 .cornerRadius(18)
                                 .foregroundColor(Color(uiColor: .systemGray))
-                            VStack(alignment: .leading, spacing: 8){
-                                Text("Guardar as roupas") //<- precisa inserir o texto variável (input da tarefa) ---> é o $prompt
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text(title)
                                     .font(.largeTitle)
                                     .fontWeight(.bold)
                                     .foregroundColor(Color(uiColor: .systemGray))
@@ -43,68 +41,72 @@ struct ResultsFocusView: View {
                     }
                     
                     Spacer()
-                    
-                    Button(action: {}) {
-                        Image(systemName: "checkmark.circle.fill")
-                            .resizable()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .padding(4)
-                            .foregroundColor(Color(uiColor: .systemGray))
-                            .overlay(content: {
-                                Circle()
-                                    .stroke(.gray, lineWidth: 4)
-                            })
-                    }
                 }
                 
-                
-                // HStack adicionar CardView + lógica do spacer fixo atrás da navbar
-                
-                // título
-                
                 HStack {
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach(steps, id: \.self){ item in
-                                HStack (alignment: .center){
-                                    ZStack (alignment: .center){
-                                        Rectangle()
-                                            .foregroundColor(Color(uiColor: .systemGray3))
-                                            .frame(width: 207, height: 526)
-                                            .cornerRadius(16)
-                                            .padding(.leading, 8)
-                                            .padding(.trailing, 8)
-                                            .alignmentGuide(.leading) { dimensions in
-                                                -dimensions.width / 2
-                                            }
-                                        
-                                        VStack(alignment: .center) {
-                                            Text(item)
-                                                .font(.body)
-                                                .foregroundColor(Color(uiColor: .black))
-                                                .multilineTextAlignment(.center)
+                    TabView {
+                        ForEach(steps.indices, id: \.self) { index in
+                            let item = steps[index]
+                            let checked = Binding(
+                                get: { checkedStates[index] },
+                                set: { checkedStates[index] = $0 }
+                            )
+                            
+                            HStack(alignment: .center) {
+                                ZStack(alignment: .center) {
+                                    Rectangle()
+                                        .foregroundColor(checked.wrappedValue ? Color(uiColor: .green) : Color(uiColor: .systemGray3))
+                                        .frame(width: 207, height: 526)
+                                        .cornerRadius(16)
+                                        .padding(.leading, 8)
+                                        .padding(.trailing, 8)
+                                        .alignmentGuide(.leading) { dimensions in
+                                            -dimensions.width / 2
                                         }
-                                        
-                                        
+                                    
+                                    VStack(alignment: .center) {
+                                        Text(item)
+                                            .font(.body)
+                                            .foregroundColor(Color(uiColor: .black))
+                                            .multilineTextAlignment(.center)
+                                            .frame(width: 207, height: 400)
+                                        Button {
+                                            checked.wrappedValue.toggle()
+                                        } label: {
+                                            Image(systemName: "checkmark.circle.fill")
+                                                .resizable()
+                                                .frame(width: 50, height: 50, alignment: .center)
+                                                .padding(4)
+                                                .foregroundColor(Color(uiColor: .systemGray))
+                                                .overlay(
+                                                    Circle()
+                                                        .stroke(Color.gray, lineWidth: 4)
+                                                )
+                                        }
                                     }
-                                    .padding(.horizontal, 16)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .background(.clear)
-                                }.ignoresSafeArea()
+                                }
+                                .padding(.horizontal, 16)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .background(Color.clear)
                             }
+                            .ignoresSafeArea()
                         }
                     }
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
                     .padding(.leading, 72.0)
                     
                     VStack(spacing: 4) {
-                        // botão de alterar view entre foco e overview
-                        NavigationLink{
-                            InventoryView(title: title, steps: steps, descs: descs)
-                        } label: {
-                            Image(systemName: "list.bullet.rectangle.portrait.fill")
-                                .font(.headline)
-                                .foregroundColor(Color(uiColor: .systemGray2))
+                        Spacer()
+                        HStack{
+                            NavigationLink(destination: InventoryView(title: title, steps: steps, descs: descs)) {
+                                Image(systemName: "list.bullet.rectangle.portrait.fill")
+                                    .font(.title2)
+                                    .foregroundColor(Color(uiColor: .gray))
+                            }
                         }
+                        Rectangle()
+                            .opacity(0.0)
+                            .frame(width: 0.1, height: 600)
                     }
                 }
                 .padding(.trailing, 20.0)
@@ -114,17 +116,8 @@ struct ResultsFocusView: View {
     }
 }
 
-struct Line: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: rect.midX, y: 0))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.height))
-        return path
-    }
-}
-
 struct ResultsFocusView_Preview: PreviewProvider {
     static var previews: some View {
-        ResultsFocusView(title: "Guardar as roupas", steps: [""], descs: [""])
+        ResultsFocusView(title: "Guardar as roupas", steps: ["Step 1", "Step 2", "Step 3"], descs: ["Desc 1", "Desc 2", "Desc 3"])
     }
 }
